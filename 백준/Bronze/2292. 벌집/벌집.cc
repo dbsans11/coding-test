5fbc09e880adf291641be67851eb986ad65fa66a
dbsans <dy061229@gmail.com>
2025-12-31 15:02:14 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.cc
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
 	int N; scanf("%d", &N);
-	int cnt = 1; N -= cnt;
-	while (N > 0) N -= cnt++ * 6;
-	printf("%d", cnt);
+	int i = 1; N--;
+	for (; N > 0; i++) N -= 6 * i;
+	printf("%d", i);
 }
