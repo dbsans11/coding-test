6ac64afe913f4110e6049ad253a6d8a034fd1d0b
dbsans <dy061229@gmail.com>
2025-12-31 14:44:47 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main() {
 	int T; scanf("%d", &T);
-	for (int i = 0; i < T; ++i) {
+	int coins[] = { 25, 10, 5, 1 };
+	while (T--) {
 		int C; scanf("%d", &C);
-		printf("%d ", C / 25); C %= 25;
-		printf("%d ", C / 10); C %= 10;
-		printf("%d ", C / 5); C %= 5;
-		printf("%d\n", C);
+		for (int i = 0; i < 4; ++i) {
+			printf("%d ", C / coins[i]);
+			C %= coins[i];
+		} printf("\n");
 	}
 }
