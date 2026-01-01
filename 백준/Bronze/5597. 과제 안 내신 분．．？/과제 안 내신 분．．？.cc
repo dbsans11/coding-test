9f773a0c6e0f49b33faf3033b1f9567ee0dcfe59
dbsans <dy061229@gmail.com>
2026-01-01 15:32:18 +0900

    Update 과제 안 내신 분．．？.cc

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -1,23 +1,7 @@
-#include <stdio.h>
-int main(void)
-{
-	int sbmt[28];
-	int i, num=1;
-
-	for(i=0;i<28;i++)
-		scanf("%d",&sbmt[i]);
-	
-	for(num=1;num<=30;num++)
-	{
-		for(i=0;i<28;i++)
-		{
-			if(num==sbmt[i])
-				break;
-			else if(i==27)
-				printf("%d\n",num);
-		}
-	}
-	
-	return 0;
-	
+#include <iostream>
+using namespace std;
+int main() {
+	int arr[30] = { 0, }; 
+	for (int i = 0; i < 28; ++i) { int temp; cin >> temp; arr[temp - 1] = 1; }
+	for (int i = 0; i < 30; i++) { if (!arr[i]) cout << i + 1 << '\n'; }
 }
