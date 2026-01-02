fd757041115b822eff4f76c2f9c816321d2019cd
dbsans <dy061229@gmail.com>
2026-01-02 13:52:07 +0900

    [Bronze II] Title: 단어의 개수, Time: 36 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string str; int cnt = 1; getline(cin, str);
+	for (int i = 0; i < str.length(); i++) if (str[i] == ' ') ++cnt;
+	if (str[0] == ' ') --cnt; if (str[str.length() - 1] == ' ') --cnt;
+	cout << cnt;
+}
