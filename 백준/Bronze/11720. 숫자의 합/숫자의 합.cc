70976054341e9a24e409826edae9d76e22add841
dbsans <dy061229@gmail.com>
2026-01-02 13:09:08 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -1,15 +1,6 @@
 #include <stdio.h>
-int main(void)
-{
-	int N, temp=0, total=0;
-	scanf("%d",&N);
-	char arr[N];
-	scanf("%s",arr);
-	
-	for(int i=0;i<N;i++)
-		total+=arr[i]-'0';
-	printf("%d",total);
-	
-	return 0;
-	
+int main() {
+	int N, res = 0; scanf("%d", &N);
+	while (N--) { int t; scanf("%1d", &t); res += t; }
+	printf("%d", res);
 }
