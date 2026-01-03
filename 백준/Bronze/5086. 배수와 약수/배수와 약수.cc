c17e93576fa7e32aabbdb6809e8b8c40a22381d5
dbsans <dy061229@gmail.com>
2026-01-03 19:17:57 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+	while (true) {
+		int A, B; scanf("%d %d", &A, &B);
+		if (A == 0 && B == 0) break;
+		if (B % A == 0) printf("factor\n");
+		else if (A % B == 0) printf("multiple\n");
+		else printf("neither\n");
+	}
+}
