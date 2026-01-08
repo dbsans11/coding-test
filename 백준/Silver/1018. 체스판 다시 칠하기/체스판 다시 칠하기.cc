a215352b212415b6e6ba5a9e96b7a6765829072e
dbsans <dy061229@gmail.com>
2026-01-08 11:30:08 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -1,68 +1,28 @@
 #include <iostream>
 using namespace std;
-
-char arr[51][51];
-char reshiram[8][8] = {
-	{'W','B','W','B','W','B','W','B'},
-	{'B','W','B','W','B','W','B','W'},
-	{'W','B','W','B','W','B','W','B'},
-	{'B','W','B','W','B','W','B','W'},
-	{'W','B','W','B','W','B','W','B'},
-	{'B','W','B','W','B','W','B','W'},
-	{'W','B','W','B','W','B','W','B'},
-	{'B','W','B','W','B','W','B','W'},
-};
-char zekrom[8][8] = {
-	{'B','W','B','W','B','W','B','W'},
-	{'W','B','W','B','W','B','W','B'},
-	{'B','W','B','W','B','W','B','W'},
-	{'W','B','W','B','W','B','W','B'},
-	{'B','W','B','W','B','W','B','W'},
-	{'W','B','W','B','W','B','W','B'},
-	{'B','W','B','W','B','W','B','W'},
-	{'W','B','W','B','W','B','W','B'},
-};
-
-int ReshiramPattern(int r, int c) {
-	int res = 0;
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			if (arr[i + r][j + c] != reshiram[i][j]) 
-				res++;
+int main() {
+	int N, M, r = 65; cin >> N >> M; char** a = new char* [N];
+	for (int i = 0; i < N; ++i) { a[i] = new char[M]; for (int j = 0; j < M; ++j) cin >> a[i][j]; }
+	char w[8][8] = { {'W','B','W','B','W','B','W','B'}, {'B','W','B','W','B','W','B','W'},
+	{'W','B','W','B','W','B','W','B'}, {'B','W','B','W','B','W','B','W'},
+	{'W','B','W','B','W','B','W','B'}, {'B','W','B','W','B','W','B','W'},
+	{'W','B','W','B','W','B','W','B'}, {'B','W','B','W','B','W','B','W'} };
+	char b[8][8] = { {'B','W','B','W','B','W','B','W'}, {'W','B','W','B','W','B','W','B'},
+		{'B','W','B','W','B','W','B','W'}, {'W','B','W','B','W','B','W','B'},
+		{'B','W','B','W','B','W','B','W'}, {'W','B','W','B','W','B','W','B'},
+		{'B','W','B','W','B','W','B','W'}, {'W','B','W','B','W','B','W','B'}, };
+	for (int i = 0; i < N - 7; ++i) {
+		for (int j = 0; j < M - 7; ++j) {
+			int t = 0; for (int x = 0; x < 8; ++x) {
+				for (int y = 0; y < 8; ++y) {
+					t += !(a[i + x][j + y] == w[x][y]);
+				}
+			} r = t < r ? t : r;
+			t = 0; for (int x = 0; x < 8; ++x) {
+				for (int y = 0; y < 8; ++y) {
+					t += !(a[i + x][j + y] == b[x][y]);
+				}
+			} r = t < r ? t : r;
 		}
-	}
-	return res;
-}
-int ZekromPattern(int r, int c) {
-	int res = 0;
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			if (arr[i + r][j + c] != zekrom[i][j])
-				res++;
-		}
-	}
-	return res;
-}
-
-int main(void)
-{
-	int row, column; cin >> row >> column;
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < column; j++) 
-			cin >> arr[i][j];
-	}
-
-	int min = 65, re, ze;
-	for (int i = 0; i <= row - 8; i++) {
-		for (int j = 0; j <= column - 8; j++) {
-			re = ReshiramPattern(i, j);
-			ze = ZekromPattern(i, j);
-			if (re < ze) min = (re < min ? re : min);
-			else min = (ze < min ? ze : min);
-		}
-	}
-
-	cout << min;
-
-	return 0;
+	} cout << r;
 }
