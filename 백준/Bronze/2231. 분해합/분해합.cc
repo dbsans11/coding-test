e52049c423fb0044f0709ceef03e741fdbd86aad
dbsans <dy061229@gmail.com>
2026-01-08 10:50:42 +0900

    [Bronze II] Title: 분해합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -1,15 +1,9 @@
 #include <iostream>
 using namespace std;
-
-int main(void)
-{
-	int inp, cnt, sum; cin >> inp;
-	for (int i = 1; i < inp; i++) {
-		cnt = i; sum = i;
-		while (cnt) { sum += cnt % 10; cnt /= 10; }
-		if (inp == sum) { cout << i; return 0; }
-	}
-	cout << 0; 
-	
-	return 0;
+int main() {
+	int N, r = 0; cin >> N;
+	for (int i = 1; i < N; ++i) {
+		int a = i, b = i; while (a) { b += a % 10; a /= 10; }
+		if (b == N) { r = i; break; }
+	} cout << r;
 }
