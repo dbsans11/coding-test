6dd85bc4d169c7b61b18e8f44f20b5c6bb17f2d6
dbsans <dy061229@gmail.com>
2026-01-08 18:01:13 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -3,26 +3,15 @@ using namespace std;
 int main() {
 	int N, M, r = 65; cin >> N >> M; char** a = new char* [N];
 	for (int i = 0; i < N; ++i) { a[i] = new char[M]; for (int j = 0; j < M; ++j) cin >> a[i][j]; }
-	char w[8][8] = { {'W','B','W','B','W','B','W','B'}, {'B','W','B','W','B','W','B','W'},
-	{'W','B','W','B','W','B','W','B'}, {'B','W','B','W','B','W','B','W'},
-	{'W','B','W','B','W','B','W','B'}, {'B','W','B','W','B','W','B','W'},
-	{'W','B','W','B','W','B','W','B'}, {'B','W','B','W','B','W','B','W'} };
-	char b[8][8] = { {'B','W','B','W','B','W','B','W'}, {'W','B','W','B','W','B','W','B'},
-		{'B','W','B','W','B','W','B','W'}, {'W','B','W','B','W','B','W','B'},
-		{'B','W','B','W','B','W','B','W'}, {'W','B','W','B','W','B','W','B'},
-		{'B','W','B','W','B','W','B','W'}, {'W','B','W','B','W','B','W','B'}, };
+	
 	for (int i = 0; i < N - 7; ++i) {
 		for (int j = 0; j < M - 7; ++j) {
-			int t = 0; for (int x = 0; x < 8; ++x) {
+			int w = 0, b = 0; for (int x = 0; x < 8; ++x) {
 				for (int y = 0; y < 8; ++y) {
-					t += !(a[i + x][j + y] == w[x][y]);
+					if ((x + y) % 2 == 0) { if (a[i + x][j + y] != 'W') ++w; else ++b; }
+					else { if (a[i + x][j + y] != 'B') ++w; else ++b; }
 				}
-			} r = t < r ? t : r;
-			t = 0; for (int x = 0; x < 8; ++x) {
-				for (int y = 0; y < 8; ++y) {
-					t += !(a[i + x][j + y] == b[x][y]);
-				}
-			} r = t < r ? t : r;
+			} r = w < r ? w : r; r = b < r ? b : r;
 		}
 	} cout << r;
 }
