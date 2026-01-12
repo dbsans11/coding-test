72b6511c7f7af48a426916a7a483828990acaff4
dbsans <dy061229@gmail.com>
2026-01-12 17:39:44 +0900

    [Silver II] Title: 좌표 압축, Time: 888 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+using namespace std;
+int main() {
+	int N; cin >> N; vector<long long> v1(N); vector<long long> v2(N); for (int i = 0; i < N; ++i) { cin >> v1[i]; v2[i] = v1[i]; }
+	sort(v2.begin(), v2.end()); v2.erase(unique(v2.begin(), v2.end()), v2.end());
+	for (int i = 0; i < N; ++i) cout << lower_bound(v2.begin(), v2.end(), v1[i]) - v2.begin() << ' ';
+}
