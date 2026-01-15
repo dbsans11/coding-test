c225a0619b84b5c1c7b1f8382245acc86fe66ec3
dbsans <dy061229@gmail.com>
2026-01-15 13:34:46 +0900

    [Silver V] Title: 숫자 카드, Time: 444 ms, Memory: 36608 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,17 +1,8 @@
 #include <iostream>
-#include <algorithm>
+#include <map>
 using namespace std;
-
-bool binary_search(int a[], int s, int k) {
-	int l = 0, h = s - 1;
-	while (l <= h) {
-		int m = (l + h) / 2; if (k == a[m]) return 1;
-		if (k < a[m]) h = m - 1; else l = m + 1;
-	} return 0;
-}
-
 int main() {
-	int N, M; cin >> N; int* a = new int[N]; for (int i = 0; i < N; ++i) cin >> a[i];
-	sort(a, a + N); cin >> M; int* r = new int[M]; for (int i = 0; i < M; ++i) cin >> r[i];
-	for (int i = 0; i < M; ++i) cout << binary_search(a, N, r[i]) << ' ';
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	int N, M; map<int, int> m; cin >> N; for (int i = 0; i < N; ++i) { int t; cin >> t; m[t] = 1; }
+	cin >> M; for (int i = 0; i < M; ++i) { int t; cin >> t; cout << m[t] << ' '; }
 }
