242efdb0005c9e17656335c9613c1edf82383570
dbsans <dy061229@gmail.com>
2026-01-15 15:51:50 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 144 ms, Memory: 20864 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -4,9 +4,10 @@
 using namespace std;
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL);
-	int N, M; map<int, string> mp1; map<string, int> mp2; cin >> N >> M; for (int i = 1; i <= N; ++i) { cin >> mp1[i]; mp2[mp1[i]] = i; }
+	int N, M; cin >> N >> M; string* a = new string[N + 1]; map<string, int> m;
+	for (int i = 1; i <= N; ++i) { cin >> a[i]; m[a[i]] = i; }
 	while (M--) {
-		string t; cin >> t; 
-		try { cout << mp2.at(t) << '\n'; } catch (out_of_range) { cout << mp1.at(stoi(t)) << '\n'; }
+		string t; cin >> t;
+		if (isdigit(t[0])) cout << a[stoi(t)] << '\n'; else cout << m[t] << '\n';
 	}
 }
