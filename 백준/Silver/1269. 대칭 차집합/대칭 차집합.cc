928babbdeb2c39fec4a4129dad47801b0f52c12e
dbsans <dy061229@gmail.com>
2026-01-15 17:12:31 +0900

    [Silver IV] Title: 대칭 차집합, Time: 64 ms, Memory: 6792 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -3,6 +3,7 @@
 #include <algorithm>
 using namespace std;
 int main() {
+	ios::sync_with_stdio(0); cin.tie(NULL);
 	int a, b; cin >> a >> b; vector<int> A(a), B(b), v; 
 	for (int i = 0; i < a; ++i) cin >> A[i]; for (int i = 0; i < b; ++i) cin >> B[i]; sort(A.begin(), A.end()); sort(B.begin(), B.end()); 
 	set_symmetric_difference(A.begin(), A.end(), B.begin(), B.end(), back_inserter(v)); cout << v.size();
