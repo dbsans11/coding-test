3bbf4e62fbf6d6da44742111aae066c9eebead3b
dbsans <dy061229@gmail.com>
2026-01-29 16:03:32 +0900

    [Bronze I] Title: 최소공배수, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int gcd(int a, int b) { if (b == 0) return a; else return gcd(b, a % b); }
+int main() {
+	int T, A, B; cin >> T;
+	while (T--) { cin >> A >> B; cout << A * B / gcd(A, B) << '\n'; }
+}
