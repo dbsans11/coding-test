cea9f6cae8f1c7c9e21668678ab2bfce847dff7b
dbsans <dy061229@gmail.com>
2026-02-23 23:31:00 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.cc
+++ 백준/Silver/13241. 최소공배수/최소공배수.cc
@@ -1,6 +1,6 @@
 #include <iostream>
+#include <numeric>
 using namespace std;
-int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
 int main() {
-	long long int a, b; cin >> a >> b; cout << a * b / gcd(a, b);
+	long long int a, b; cin >> a >> b; cout << lcm(a, b);
 }
