e294e0994b6997fd8d36e181628f09f781bdb0cf
dbsans <dy061229@gmail.com>
2026-02-24 13:56:22 +0900

    [Silver III] Title: 소수 구하기, Time: 16 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -2,7 +2,10 @@
 using namespace std;
 int a[1000001] = { 0, 1 };
 int main() {
-	int m, n; 
-	for (int i = 2; i <= 1000001; ++i) for (int j = 2; i*j <= 1000001; ++j) a[i * j] = 1;
+	int m, n;
+		for (int i = 2; i <= 1000001; ++i) {
+			if (a[i]) continue;
+			for (int j = 2; i * j <= 1000001; ++j) a[i * j] = 1;
+		}
 	cin >> m >> n; for (int i = m; i <= n; ++i) if (!a[i]) cout << i << '\n';
 }
