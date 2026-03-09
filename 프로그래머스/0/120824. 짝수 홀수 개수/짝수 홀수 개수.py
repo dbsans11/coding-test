903479f1f3463241f8b17c185a79cb9289f42a91
dbsans <dy061229@gmail.com>
2026-03-09 18:27:11 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,3 +1,5 @@
 def solution(numlist):
-    n = len([x for x in numlist if x %2==0])
-    return [n, len(numlist)-n]
+    answer = [0,0]
+    for n in numlist:
+        answer[n%2]+=1
+    return answer
