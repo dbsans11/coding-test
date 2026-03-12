cb4042fb6d9d50a1f0f5fe62c2e0192f085dc46f
dbsans <dy061229@gmail.com>
2026-03-12 14:42:38 +0900

    [level 0] Title: 약수 구하기, Time: 0.60 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,1 +1,5 @@
-solution = lambda n: [i for i in range(1,n+1) if n%i==0]
+def solution(n):
+    a = set()
+    for i in range(1, int(n**(0.5))+1):
+        if n%i==0: a.update([i, n//i])
+    return sorted(list(a))
