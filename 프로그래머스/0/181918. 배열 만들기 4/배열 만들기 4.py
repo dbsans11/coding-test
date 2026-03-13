1b0b5fff1e684413da5a386b0cb3cd4abe0087fc
dbsans <dy061229@gmail.com>
2026-03-13 13:56:32 +0900

    [level 0] Title: 배열 만들기 4, Time: 53.13 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -1,8 +1,3 @@
-def solution(a):
-    s, i=[], 0
-    while i<len(a):
-        if not s or s[-1] < a[i]:
-            s.append(a[i])
-            i+=1
-        else: s.pop()
-    return s
+def solution(arr):
+    stk = []
+    return stk
