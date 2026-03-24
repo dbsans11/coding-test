bdb430f74a7ac01f1053fb7c9b423c8917bc2d54
dbsans <dy061229@gmail.com>
2026-03-24 18:48:11 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,7 +1,3 @@
-def solution(keys,b):
-    d={'up':[0,1],'down':[0,-1],'left':[-1,0],'right':[1,0]}
-    limx,limy,nx,ny = b[0]//2,b[1]//2,0,0
-    for k in keys:
-        tx,ty=d[k]
-        nx,ny=min(limx,max(-limx,nx+tx)),min(limy,max(-limy,ny+ty))
-    return [nx,ny]
+def solution(keyinput, board):
+    answer = []
+    return answer
