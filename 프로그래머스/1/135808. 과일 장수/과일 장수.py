6cf596712bb706699a65a41526fb1d875b744a8c
dbsans <dy061229@gmail.com>
2026-04-08 11:57:40 +0900

    [level 1] Title: 과일 장수, Time: 94.82 ms, Memory: 28.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,7 +1,1 @@
-def solution(k,m,score):
-    score.sort(reverse=1)
-    profit,idx,ln=0,0,len(score)
-    while idx+m<=ln: 
-        profit+=min(score[idx:idx+m])*m
-        idx+=m
-    return profit
+solution = lambda k,m,s:sum(sorted(s)[-m::-m]*m)
