19f266f12164ca93dedd461e1a16eb61cf962681
dbsans <dy061229@gmail.com>
2026-04-09 13:13:18 +0900

    [level 1] Title: 소수 찾기, Time: 29.84 ms, Memory: 22.6 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -2,7 +2,5 @@ def solution(n):
     e=[1]*(n+1)
     e[0]=e[1]=0
     for i in range(2,int(n**0.5)+1):
-        if e[i]:
-            for j in range(i*i,n+1,i):
-                e[j]=0
-    return e.count(1)
+        if e[i]: e[i*i:n+1:i]=[0]*len(range(i*i,n+1,i))
+    return sum(e)
