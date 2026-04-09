9f072796d9f2d68ccc81d423482c249476c61eca
dbsans <dy061229@gmail.com>
2026-04-09 14:43:13 +0900

    [level 1] Title: 옹알이 (2), Time: 0.09 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,9 +1,6 @@
 def solution(bab):
     r=0
     for b in bab:
-        b=b.replace('aya','1').replace('ye','2').replace('woo','3').replace('ma','4')
-        if max(b) not in '1234': continue
-        for i,v in enumerate(b[1:]): 
-            if b[i]==v: break
-        else: r+=1
+        if 'ayaaya' in b or 'yeye' in b or 'woowoo' in b or 'mama' in b: continue
+        if not b.replace('aya', ' ').replace('ye', ' ').replace('woo', ' ').replace('ma',' ').strip(): r+=1
     return r
