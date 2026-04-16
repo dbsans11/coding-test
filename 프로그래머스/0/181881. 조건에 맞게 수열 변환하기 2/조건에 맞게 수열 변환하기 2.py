7d6e400035a07c617c014dcf01d695bd7f7b73f0
dbsans <dy061229@gmail.com>
2026-04-16 20:45:33 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 708.89 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -1,7 +1,7 @@
 def solution(a):
-    x=1
+    cnt=0
     while 1:
-        t = list(map(lambda v: v/2 if v>=50 and v%2==0 else v*2+1 if v<50 and v%2 else v, a))
-        if t==a: return x-1
-        x+=1
+        t = list(map(lambda v: v//2 if v>=50 and v%2==0 else v*2+1 if v<50 and v%2 else v, a))
+        if t==a: return cnt 
         a=t
+        cnt+=1
