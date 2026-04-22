03a64b05bddd8e0760d065a4b65cef48c2bc6bab
dbsans <dy061229@gmail.com>
2026-04-22 11:59:02 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.02 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -1,6 +1,6 @@
-def solution(n):
-    t=[n]
-    while(n>1): 
-        n=n//2 if n%2==0 else 3*n+1
-        t.append(n)
-    return t
+def solution(x):
+    r = [x]
+    while x!=1:
+        x = 3*x+1 if x%2 else x//2
+        r.append(x)
+    return r
