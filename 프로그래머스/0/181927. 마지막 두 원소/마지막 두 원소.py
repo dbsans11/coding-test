ba7b3f80999dee47d3857f812e1ad88533076d3d
dbsans <dy061229@gmail.com>
2026-04-23 13:17:31 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,1 +1,2 @@
-solution = lambda num_list: num_list + [num_list[-1] - num_list[-2] if num_list[-1] > num_list[-2] else num_list[-1] * 2]
+def solution(num):
+    return num +[num[-1]-num[-2] if num[-1]>num[-2] else num[-1]*2]
