538c32e4e9a7ce5ad599fe1b855e681bc26039a4
dbsans <dy061229@gmail.com>
2026-04-23 13:06:38 +0900

    [level 0] Title: 다항식 더하기, Time: 0.03 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -1,9 +1,9 @@
 def solution(p):
     x,n=0,0
-    for v in p.split(' + '):
-        if v.endswith('x'): x+= (int(v[:-1]) if v!='x' else 1)
-        else: n+=int(v)
-    res = []
-    if x: res.append(f'{x}x' if x!=1 else 'x')
-    if n: res.append(f'{n}')
-    return ' + '.join(res)
+    for c in p.split(' + '):
+        if c.endswith('x'): x += int(c[:-1]) if c!='x' else 1
+        else: n+=int(c)
+    r=[]
+    if x: r.append(f'{x}x' if x!=1 else 'x')
+    if n: r.append(str(n))
+    return ' + '.join(r)
