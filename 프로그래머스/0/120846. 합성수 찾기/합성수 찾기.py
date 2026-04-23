9c1db371943deae63195173c8782ef464b116fa8
dbsans <dy061229@gmail.com>
2026-04-23 13:29:03 +0900

    [level 0] Title: 합성수 찾기, Time: 0.11 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,6 +1,8 @@
 def solution(n):
-    p = [0]*(n+1)
-    for i in range(2, int(n**0.5)+1):
-        if not p[i]:
-            for j in range(i*i, n+1, i): p[j] =1
-    return sum(p)
+    r=0
+    for i in range(4,n+1):
+        for j in range(2,int(i**(0.5))+1):
+            if i%j==0:
+                r+=1
+                break
+    return r
