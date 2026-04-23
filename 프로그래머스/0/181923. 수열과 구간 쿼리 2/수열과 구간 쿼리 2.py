566c84ba5aa43014469a7d865dd548d64fc0d07a
dbsans <dy061229@gmail.com>
2026-04-23 12:20:15 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 8.53 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,1 +1,5 @@
-solution = lambda a, q: [min([x for x in a[s:e+1] if x>k], default=-1) for s,e,k in q]
+def solution(a,q):
+    r=[]
+    for s,e,k in q:
+        r.append(min([v for v in a[s:e+1] if v>k],default=-1))
+    return r
