fee90971aad35a516979222b5729194e4742c33a
dbsans <dy061229@gmail.com>
2026-04-23 12:51:17 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,6 +1,6 @@
-def solution(s):
-    s=[x+y for x,y in s]
-    r = {}
-    for i,v in enumerate(sorted(s,reverse=1)):
-        if v not in r: r[v]=i+1
-    return [r[v] for v in s]
+def solution(score):
+    score=[x+y for x,y in score]
+    rank={}
+    for i,v in enumerate(sorted(score, reverse=1)):
+        if v not in rank: rank[v]=i+1
+    return [rank[v] for v in score]
