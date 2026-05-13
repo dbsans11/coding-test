3b9ded41e73e40f98b53619c71520ad046fffd72
dbsans <dy061229@gmail.com>
2026-05-13 09:04:46 +0900

    [level 1] Title: 유연근무제, Time: 1.23 ms, Memory: 9.55 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -1,13 +1,11 @@
 def solution(schedules, timelogs, startday):
     res=0
     for sch, tlg in zip(schedules, timelogs):
-        sch, temp=str(sch), 1
-        sch=int(sch[:-2])*60+int(sch[-2:])+10
+        sch, temp=sch//100*60+sch%100+10, 1
         for d,t in enumerate(tlg, start=startday):
             d%=7
             if d==6 or d==0: continue
-            t=str(t)
-            t=int(t[:-2])*60+int(t[-2:])
+            t=t//100*60+t%100
             if t>sch:
                 temp=0
                 break
