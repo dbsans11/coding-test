df4e8c58d8a5c1748dd18874fad4ae769e2d1fe7
dbsans <dy061229@gmail.com>
2026-05-21 15:40:43 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,11 +1,7 @@
 def solution(n, w, num):
-    num_y = (num-1)//w
+    top_y, num_y = (n-1)//w, (num-1)//w
+    top_x = w-1-(n-1)%w if top_y%2 else (n-1)%w
     num_x = w-1-(num-1)%w if num_y%2 else (num-1)%w
-    a=0
     
-    while 1:
-        next_num = (num_y+a) * w + (w - num_x if (num_y+a)%2 else num_x + 1)
-        if next_num > n: break
-        else: a+=1
-    
-    return a
+    if top_y%2: return top_y-num_y + (num_x >= top_x)*1
+    else: return top_y-num_y + (num_x <= top_x)*1
