88b39afaff6816359529674cf4ef1fbf6e306e4c
dbsans <dy061229@gmail.com>
2026-06-18 12:44:45 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.09 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,17 +1,21 @@
 def solution(today, terms, privacies):
-    today=list(map(int, today.split('.')))
-    todays=today[0]*28*12 + today[1]*28 + today[2]
+    def date_to_int(date_str):
+        y, m, d = map(int, date_str.split('.'))
+        return y*28*12 + m*28 + d
     
-    terms_dict={}
+    answer = []
+    today = date_to_int(today)
+    
+    term_dict = {}
     for term in terms:
         k, v = term.split()
-        terms_dict[k] = int(v)*28
+        term_dict[k] = int(v)*28
     
-    res=[]
-    for idx, pri in enumerate(privacies, start=1):
+    for i, pri in enumerate(privacies, start=1):
         date, term = pri.split()
-        date = list(map(int,date.split('.')))
-        days = date[0]*28*12 + date[1]*28 + date[2] + terms_dict[term] - 1
-        if days < todays: res.append(idx)
+        date = date_to_int(date) + term_dict[term]
+        
+        if today >= date:
+            answer.append(i)
     
-    return res
+    return answer
