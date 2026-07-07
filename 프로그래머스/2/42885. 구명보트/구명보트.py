dd429a960266115f6eecfaa4c4aabfe1c5e32abd
dbsans <dy061229@gmail.com>
2026-07-07 11:03:09 +0900

    [level 2] Title: 구명보트, Time: 7.42 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,17 @@
+def solution(people, limit):
+    answer, start, end = 0, 0, len(people)-1
+    
+    people.sort()
+    while start<end:
+        if people[start] + people[end] <= limit:
+            start+=1
+            end-=1
+            answer+=1
+        else:
+            end-=1
+            answer+=1
+    
+    if start == end:
+        answer += 1
+    
+    return answer
